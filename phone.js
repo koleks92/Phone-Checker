ab5d8e300634e3d640b6a263d75f451972862704
koleks92 <koleks92@gmail.com>
2023-02-15 22:59:49 +0100

    first commit

---
+++ phone.js
@@ -0,0 +1,8 @@
+function telephoneCheck(str) {
+  let regex = /^(1[\s]?)?((?:[(](?:\d\d\d|\d\d\d)[)][\s]?)|(?:(?:\d\d\d|\d\d\d)[\s.-]?)){1}(\d\d\d|\d\d\d|\d\d{2}[\s.-]?){1}(\d{4}){1}$/
+  if (regex.test(str))
+    return true;
+  return false;
+}
+
+telephoneCheck("555-555-5555");
